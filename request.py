d1ff59f5bab904bbaf7e72d3f8ceae3827bad156
feloro <feloro@users.noreply.github.com>
2024-01-19 20:12:59 +0300

    command /soon is introduced

--- request.py
+++ request.py
@@ -1,8 +1,8 @@
 import requests
 
 from pydantic import BaseModel, validator
-from typing import Optional, List
-from datetime import date
+from typing import Optional, List, overload
+from datetime import date, datetime
 
 class Competitor(BaseModel):
     isHomeCompetitor: bool
@@ -12,6 +12,8 @@ class Competitor(BaseModel):
 
 class VideoLinks(BaseModel):
     videoM3U8: str
+    videoForWebview: str
+    videoIdFrame: str
 
 class Game(BaseModel):
     competitionId: int
@@ -23,13 +25,22 @@ class Game(BaseModel):
 
     def videoUrl(self):
         if self.video is not None:
-            return self.video.videoM3U8 or None
+            baseUrl = self.video.videoForWebview.replace(self.video.videoIdFrame,"")
+            path = self.video.videoM3U8.split("/start/",1)[1]
+            path = path.replace(".m3u8","")
+            return baseUrl+path+"?width=100%25&height=100%25&lang=ru&new_html5=1" or None
+        else:
+            return None
+            
+    def startTime(self):
+        if self.matchTimeMsk is not None:
+            return datetime.strptime(self.matchTimeMsk, "%Y-%m-%d %H:%M:%S").strftime("%H:%M %d.%m")
         else:
             return None
 
 def downloadGames():
     games = []
-    url = "https://api.vtb-league.com/a49755699622d8fe2d87e6d2ac24a7bb/v1/competitions/games?limit=500"
+    url = "https://api.vtb-league.com/a49755699622d8fe2d87e6d2ac24a7bb/v1/competitions/games"
     payload = {"limit": 500}
     response = requests.get(url, params=payload)
     response_json = response.json()
@@ -41,14 +52,7 @@ def downloadGames():
 def getFinishedGames(games):
     return list(filter(lambda game: game.matchStatus == "COMPLETE", games))
 
-def getGames(date, games):
-    return list(filter(lambda game: game.matchTimeMsk.startswith(date), games))
-
-
-
-# games = downloadGames()
-# print(len(getFinishedGames(games)))
-
-# today = date.today()
-# print(len(getGames(today.strftime("%Y-%m-%d"), games)))
-# print(len(games))
+def getGames(startDate, endDate, games):
+    return list(filter(lambda game:
+        startDate <= datetime.strptime(game.matchTimeMsk, "%Y-%m-%d %H:%M:%S").date() <= endDate,
+    games))
