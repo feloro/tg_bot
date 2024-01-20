1c6fe408802a8942b9f1a39733ab46a5246b908f
feloro <istomin@saject.ru>
2024-01-20 10:35:18 +0300

    /help is fixed

--- bot.py
+++ bot.py
@@ -2,9 +2,11 @@ import os
 import telebot
 import request
 from datetime import date, timedelta
-import sys
+import time
 
 bot = telebot.TeleBot(os.environ.get('BOT_TOKEN'))
+myUser = ""
+
                          
 @bot.message_handler(commands=['today'])
 def send_today_games(message):
@@ -13,6 +15,9 @@ def send_today_games(message):
     responseText = "Игры на сегодня:"
     responseText += formatGames(request.getGames(today, today, games), False)
     bot.send_message(message.from_user.id, responseText, parse_mode= 'Markdown')
+    myUser = message.from_user.id
+    time.sleep(3)
+    bot.send_message(message.from_user.id,"Спишь?")
     
 @bot.message_handler(commands=['soon'])
 def sendSoonGames(message):
@@ -29,14 +34,14 @@ def sendTodayGames(message):
     today = date.today()
     startDate = today + timedelta(days=-5)
     responseText = "Игры за прошедшие 5 дней:"
-    responseText += formatGames(request.getGames(startDate, today, games), True).replace('-', '\-').replace('.', '\.')
+    responseText += formatGames(request.getGames(startDate, today, games), True).replace('-', r'\-').replace('.', r'\.')
     bot.send_message(message.from_user.id, responseText, parse_mode= 'MarkdownV2')
 
 @bot.message_handler(commands=['help'])
 def sendHelp(message):
     games = request.downloadGames()
     today = date.today()
-    responseText = """Для того, чтобы получить список матчей на сегодня - /today\nCписок матчей на 5 дней вперед - /soon"""
+    responseText = """Для того, чтобы получить список матчей на сегодня - /today\nCписок матчей на 5 дней вперед - /soon\nСписок матчей за последние 5 дней - /past"""
     bot.send_message(message.from_user.id, responseText)
 
 @bot.message_handler(content_types=['text'])
@@ -52,8 +57,6 @@ def formatGames(games, withScore):
             videoUrl = "[Ссылка]({})".format(game.videoUrl())
         else:
             videoUrl = "Отсутствует"
-        homeCompetitor: Competitor
-        guestCompetitor: Competitor
         if game.competitors[0].isHomeCompetitor:
             homeCompetitor = game.competitors[0]
             guestCompetitor = game.competitors[1]
